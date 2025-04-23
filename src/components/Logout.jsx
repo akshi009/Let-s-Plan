a803bfb3f36cf7bb205e8ee06e8b6d33c4c8c3c9
akshi <akshi20jain03@gmail.com>
2025-04-23 12:20:44 +0530

    loading:logout

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import auth from '../appwrite/auth'
 import { logout } from '../store/authstore'
 
 function Logout() {
+    const [loading,setLoading] = useState(false)
     const dispatch = useDispatch()
     const logoutbutton = async()=>{
+        setLoading(true)
         try {
             auth.logout().then(()=> dispatch(logout()))
         } catch (error) {
             console.log(error,': error in logout')
+        }
+        finally{
+            setLoading(false)
         }
     }
 
@@ -17,7 +22,7 @@ function Logout() {
     <button
     className="px-4 py-4 z-10  bottom-10 bg-gray-800 w-full  hover:bg-red-500 duration-300 ease-in-out transition cursor-pointer"
      onClick={logoutbutton}
-     >Logout
+     >{loading?<div>Loading...</div>:<div>Logout</div>}
      </button>
   )
 }
